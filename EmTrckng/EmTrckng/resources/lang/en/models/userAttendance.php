b259bf9a6dcc469ba8401acd135309b2a738d7be
mayurnayee303135 <97111335+mayurnayee303135@users.noreply.github.com>
2022-10-24 02:29:43 +0530

    Create userAttendance.php

---
+++ EmTrckng/EmTrckng/resources/lang/en/models/userAttendance.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | user attendance Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during user attendance for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'header' => [
+        'index' => 'User Attendance',
+        'create' => 'Create User Attendance',
+        'edit' => 'Edit User Attendance',
+        'detail' => 'User Attendance Details'
+    ],
+    'fields' => [
+        'user_id' => 'User name',
+        'check_in_date' => 'Check In Date',
+        'check_in_time' => 'Check In Time',
+        'check_out_date' => 'Check Out Date',
+        'check_out_time' => 'Check Out Time',
+        'user_location' => 'User Location'
+    ],
+    'button' => [
+        'add_new' => 'Add New',
+        'update' => 'Update',
+        'save' => 'Save',
+        'cancel'=>'Cancel',
+        'back'=>'Back'
+    ],
+    'message' => [
+        'not_found' => 'User Attendance not found',
+        'confirm_delete' => 'Are you sure?',
+        'create_success' => 'User Attendance saved successfully.',
+        'update_success' => 'User Attendance updated successfully.',
+        'delete_success' => 'User Attendance deleted successfully.'
+    ]
+];
