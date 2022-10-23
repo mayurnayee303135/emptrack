a45acc78b77433ba6f66705639a62c1afe8a7134
mayurnayee303135 <97111335+mayurnayee303135@users.noreply.github.com>
2022-10-23 12:28:39 +0530

    Update UserController.php

--- EmTrckng/EmTrckng/app/Http/Controllers/API/UserController.php
+++ EmTrckng/EmTrckng/app/Http/Controllers/API/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\CheckInOut;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -44,6 +45,49 @@ class UserController extends Controller
         else
         {
             return response()->json(['data' => [] , 'message'=>'User id does not exists.'], 200); 
+        }
+    }
+    
+    public function checkInOut(Request $request)
+    {
+        $userId = $request->user_id;
+        $date = $request->date;
+        $status = $request->status;
+        $checkIn = $request->check_in;
+        $checkOut = $request->check_out;
+        $latitude = $request->latitude;
+        $longitude = $request->longitude;
+        $address = $request->address;
+
+        $checkData = CheckInOut::where('user_id','=',$userId)->where('date','=',$date)->select('id')->first();
+
+        if(!empty($checkData->id))
+        {
+            $checkInOut = CheckInOut::findOrFail($checkData->id);
+            $checkInOut->user_id = $userId;
+            $checkInOut->date = $date;
+            $checkInOut->flag = 1;
+            $checkInOut->check_in = $checkIn;
+            $checkInOut->latitude = $latitude;
+            $checkInOut->longitude = $longitude;
+            $checkInOut->address = $address;
+            $checkInOut->update();
+
+            return response()->json(['data' => $checkInOut->id , 'message' => 'User Check Out Successfull.'], 200);
+        }
+        else
+        {
+            $checkInOut = new CheckInOut();
+            $checkInOut->user_id = $userId;
+            $checkInOut->date = $date;
+            $checkInOut->flag = 0;
+            $checkInOut->check_in = $checkIn;
+            $checkInOut->latitude = $latitude;
+            $checkInOut->longitude = $longitude;
+            $checkInOut->address = $address;
+            $checkInOut->save();
+
+            return response()->json(['data' => $checkInOut->id , 'message' => 'User Check In Successfull.'], 200);
         }
     }
 }
