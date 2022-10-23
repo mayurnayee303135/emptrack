3a7c8b028717b5ded2b270985ce6a8e477d8f6bf
mayurnayee303135 <97111335+mayurnayee303135@users.noreply.github.com>
2022-10-24 02:28:33 +0530

    Create UserAttendanceRepository.php

---
+++ EmTrckng/EmTrckng/app/Repositories/UserAttendanceRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\UserAttendance;
+use App\Repositories\BaseRepository;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class UserAttendanceRepository
+ * @package App\Repositories
+ * @version July 26, 2021, 12:17 pm UTC
+ */
+
+class UserAttendanceRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'user_id',
+        'check_in_date',
+        'check_out_date',
+        'user_location',
+        'check_in_time',
+        'check_out_time'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return UserAttendance::class;
+    }
+}
