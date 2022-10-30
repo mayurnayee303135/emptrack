e84acc12eb3774cbf1fb848b37b573e062410cc4
mayurnayee303135 <97111335+mayurnayee303135@users.noreply.github.com>
2022-10-31 01:10:54 +0530

    Update show_fields.blade.php

--- EmTrckng/EmTrckng/resources/views/leads/show_fields.blade.php
+++ EmTrckng/EmTrckng/resources/views/leads/show_fields.blade.php
@@ -142,11 +142,11 @@
                                     @if (!empty($attchment->attachment))
                                         @php  $explodeName = explode('.',$attchment->attachment);@endphp
                                         @if($explodeName[1] == "jpg" || $explodeName[1] == "jpeg" || $explodeName[1] == "gif")
-                                            <td><img src="{{ url('leadAttachments/'.$attchment->attachment) }}" class="rounded-circle img-circle" width="100" height="100"><a href="{{ url('leadAttachments/'.$attchment->attachment) }}">{{ url('leadAttachments/'.$attchment->attachment) }}</a></td>
+                                            <td><a href="{{ url('leadAttachments/'.$attchment->attachment) }}"><img src="{{ url('leadAttachments/'.$attchment->attachment) }}" width="100" height="100"></a></td>
                                         @elseif($explodeName[1] == "pdf")
-                                            <td><i class=" btn-danger fas fa-file-pdf fa-4x"></i><a href="{{ url('leadAttachments/'.$attchment->attachment) }}">{{ url('leadAttachments/'.$attchment->attachment) }}</a></td>
+                                            <td><a href="{{ url('leadAttachments/'.$attchment->attachment) }}"><i class=" btn-danger fas fa-file-pdf fa-4x"></i></a></td>
                                         @elseif($explodeName[1] == "xls" || $explodeName[1] == "xlsx" || $explodeName[1] == "xlsm" || $explodeName[1] == "xlsb" || $explodeName[1] == "xltx" ||  $explodeName[1] == "xml")
-                                            <td><i class=" btn-success fas fa-file-excel fa-4x"></i><a href="{{ url('leadAttachments/'.$attchment->attachment) }}">{{ url('leadAttachments/'.$attchment->attachment) }}</a></td>
+                                            <td><a href="{{ url('leadAttachments/'.$attchment->attachment) }}"><i class=" btn-success fas fa-file-excel fa-4x"></i></a></td>
                                         @else
                                         <td> NA </td>
                                         @endif
