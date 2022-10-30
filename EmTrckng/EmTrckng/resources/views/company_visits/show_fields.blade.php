50df9e994eb26e0c372256f9dee62ed80ab4535c
mayurnayee303135 <97111335+mayurnayee303135@users.noreply.github.com>
2022-10-30 23:39:48 +0530

    Update show_fields.blade.php

--- EmTrckng/EmTrckng/resources/views/company_visits/show_fields.blade.php
+++ EmTrckng/EmTrckng/resources/views/company_visits/show_fields.blade.php
@@ -107,7 +107,7 @@
                                     @if (!empty($attchment->image))
                                         @php  $explodeName = explode('.',$attchment->image);@endphp
                                         @if($explodeName[1] == "jpg" || $explodeName[1] == "jpeg" || $explodeName[1] == "gif")
-                                            <td><img src="{{ url('companyAttachments/'.$attchment->image) }}" class="rounded-circle img-circle" ><a href="{{ url('companyAttachments/'.$attchment->image) }}">{{ url('companyAttachments/'.$attchment->image) }}</a></td>
+                                            <td><img src="{{ url('companyAttachments/'.$attchment->image) }}" class="rounded-circle img-circle" width="100" height="100"><a href="{{ url('companyAttachments/'.$attchment->image) }}">{{ url('companyAttachments/'.$attchment->image) }}</a></td>
                                         @elseif($explodeName[1] == "pdf")
                                             <td><i class=" btn-danger fas fa-file-pdf fa-4x"></i><a href="{{ url('companyAttachments/'.$attchment->image) }}">{{ url('companyAttachments/'.$attchment->image) }}</a></td>
                                         @elseif($explodeName[1] == "xls" || $explodeName[1] == "xlsx" || $explodeName[1] == "xlsm" || $explodeName[1] == "xlsb" || $explodeName[1] == "xltx" ||  $explodeName[1] == "xml")
