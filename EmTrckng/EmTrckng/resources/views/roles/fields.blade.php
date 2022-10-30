bf5e57d9054fe75f59d1f989693d2f20b45dd806
mayurnayee303135 <97111335+mayurnayee303135@users.noreply.github.com>
2022-10-30 23:33:01 +0530

    Update fields.blade.php

--- EmTrckng/EmTrckng/resources/views/roles/fields.blade.php
+++ EmTrckng/EmTrckng/resources/views/roles/fields.blade.php
@@ -15,7 +15,7 @@
         <!-- Guard Name Field -->
         <div class="form-group col-sm-6">
             {!! Form::label('guard_name', 'Guard Name:') !!}
-            {!! Form::select('guard_name', ['web' => 'web', 'api' => 'api'], null, ['class' => 'form-control custom-select']) !!}
+            {!! Form::select('guard_name', ['web' => 'web'], null, ['class' => 'form-control custom-select']) !!}
         </div>
 
 
